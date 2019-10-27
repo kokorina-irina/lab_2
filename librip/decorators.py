bdb03af3285c3a0341c761874f89cda211ce29e5
kokorina-irina <57055429+kokorina-irina@users.noreply.github.com>
2019-10-27 08:48:27 +0300

    выполнено

--- librip/decorators.py
+++ librip/decorators.py
@@ -35,3 +35,18 @@
 # test_4
 # 1
 # 2
+def print_result(func):
+    def decorated_func(*args):
+        print(func.__name__)
+        result = func(*args)
+        if isinstance(result, list):
+            for item in result:
+                print(item)
+        elif isinstance(result, dict):
+            for k, v in result.items():
+                print(k, "=", v)
+        else:
+            print(result)
+        return result
+
+    return decorated_func
