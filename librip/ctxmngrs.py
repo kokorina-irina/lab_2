52030b39411ce07a3af3b00a9993d7bc2a7427e8
kokorina-irina <57055429+kokorina-irina@users.noreply.github.com>
2019-10-27 08:47:40 +0300

    выполнено

--- librip/ctxmngrs.py
+++ librip/ctxmngrs.py
@@ -6,3 +6,13 @@
 #   sleep(5.5)
 #
 # После завершения блока должно вывестись в консоль примерно 5.5
+import time
+
+
+class timer:
+
+    def __enter__(self):
+        self.t = time.clock()
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        print (time.clock() - self.t)
