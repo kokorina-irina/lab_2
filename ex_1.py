097f2cf5e80f22f8bf2706bb8bc60f65191ab0cf
kokorina-irina <57055429+kokorina-irina@users.noreply.github.com>
2019-10-27 08:33:12 +0300

    Update ex_1.py

--- ex_1.py
+++ ex_1.py
@@ -1,11 +1,13 @@
-#!/usr/bin/env python3
-from librip.gens import field
+#!/usr / bin / env python3
+из librip.поле gens импорт
 
-goods = [
-    {'title': 'Ковер', 'price': 2000, 'color': 'green'},
-    {'title': 'Диван для отдыха', 'price': 5300, 'color': 'black'},
-    {'title': 'Стелаж', 'price': 7000, 'color': 'white'},
-    {'title': 'Вешалка для одежды', 'price': 800, 'color': 'white'}
+товары = [
+ {'title': 'Ковер', 'price': 2000, 'color': 'green'},
+ {'title': 'Диван для отдыха', 'price': 5300, 'color': 'black'},
+ {'title': 'Стелаж', ' Price ': 7000, 'color': 'white'},
+ {'title': 'Вешалка для одежды', 'price': 800, 'color': 'white'}
 ]
 
-# Реализация задания 1
+# Реализация задания
+печать (*поле (товар, 'Название', 'Цена'), sep =', ')
+печать (*поле (товар, 'название'), sep  =', ')
