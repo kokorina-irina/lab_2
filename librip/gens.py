b8b2a49a8fd06b7e7d909faf25caf2daf65f31fe
kokorina-irina <57055429+kokorina-irina@users.noreply.github.com>
2019-10-27 08:49:23 +0300

    выполнено

--- librip/gens.py
+++ librip/gens.py
@@ -10,9 +10,18 @@ import random
 # field(goods, 'title') должен выдавать 'Ковер', 'Диван для отдыха'
 # field(goods, 'title', 'price') должен выдавать {'title': 'Ковер', 'price': 2000}, {'title': 'Диван для отдыха', 'price': 5300}
 
+
 def field(items, *args):
     assert len(args) > 0
-    # Необходимо реализовать генератор 
+    if len(args) == 1:
+        for item in items:
+            if args[0] in item:
+                yield item[args[0]]
+    else:
+        for item in items:
+            d = {arg: item[arg] for arg in args if arg in item}
+            if len(d) > 0:
+                yield d
 
 
 # Генератор списка случайных чисел
@@ -20,5 +29,5 @@ def field(items, *args):
 # gen_random(1, 3, 5) должен выдать примерно 2, 2, 3, 2, 1
 # Hint: реализация занимает 2 строки
 def gen_random(begin, end, num_count):
-    pass
-    # Необходимо реализовать генератор
+    for i in range(num_count):
+        yield random.randint(begin, end)
